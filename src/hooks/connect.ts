aa24f8c29686d8e4a7da301e483bfa96140b8751
vill4lobos <vill4lobos@protonmail.com>
2024-03-01 23:58:58 -0300

    Move old files to new repo

---
+++ src/hooks/connect.ts
@@ -0,0 +1,227 @@
+import { useState, useEffect } from 'react';
+import { Storage, Drivers } from '@ionic/storage';
+
+export function Connect() {
+
+    const [notes, setNotes] = useState<Note[]>([]);
+    const [usedDB, setUsedDB] = useState(false);
+    const [currentFilter, setCurrentFilter] = useState('');
+    const [labels, setLabels] = useState<Array<string>>([]);
+
+    const sampleNotes = [
+        "Fazer compras no mercado",
+        "Lavar o carro",
+        "Assistir novo filme",
+        "Comprar terno para o casamamento do fiote",
+        "Ligar para o tio"
+    ]
+
+    useEffect(() => {
+        const initNotes = async () => {
+            await getNotes()
+        }
+        initNotes()
+    }, [])
+
+    useEffect (() => {
+        const updateLabels = async () => {
+            await getLabels()
+        }
+        updateLabels()
+    }, [])
+
+    useEffect(() => {
+        const updateNotes = async () => {
+            await getNotes()
+        }
+        updateNotes()
+        setUsedDB(false)
+    }, [usedDB])
+
+    const connectDB = async () => {
+        const db = new Storage({
+            name: "__teste",
+            driverOrder: [Drivers.LocalStorage, Drivers.IndexedDB]
+        })
+        await db.create()
+        return db
+    }
+
+    const addNote = async (note: string) => {
+        const db = await connectDB()
+        let key = (await db.length() + 1).toString()
+        let tempNote = 
+            { 
+                note: note,
+                priority: generateRandomInt(3),
+                status: Status.Pendent,
+                date: new Date(2024, 0, 
+                        generateRandomInt(30)),
+                labels: [],
+            }
+        db.set(key, tempNote)
+        setUsedDB(true)
+        //setNotes([...notes, createNote(tempNote, key)])
+    }
+
+    const completeNote = async (id: string) => {
+        let note = await getNote(id)
+        note.status = Status.Completed
+
+        await updateNote(id, note)
+    }
+
+    const updateNote = async (id: string, note: NoteDB) => {
+        let tempNote = await getNote(id)
+        
+        note.date = tempNote.date
+
+        const db = await connectDB()
+        db.set(
+            id,
+            note
+        )
+        setUsedDB(true)
+    }
+
+    const getNote = async (id: string) : Promise<NoteDB> => {
+        const db = await connectDB()
+        return await db.get(id)
+    }
+
+    const populate = async () => {
+        for (let i = 0; i < 40; i++) {
+            await addNote(sampleNotes[generateRandomInt(5)])
+        }
+    }
+
+    const truncate = async (pop?: boolean) => {
+        const db = await connectDB()
+        db.clear()
+
+        if (pop == true)
+            populate()
+    }
+
+   const createNote = (note: NoteDB, key?: string) => {
+    let noteDate = new Date(note.date)
+    let dayMonth: [number, number] =
+        [noteDate.getDate(), noteDate.getMonth()]
+
+    return {
+        key: key,
+        note: note.note,
+        priority: note.priority,
+        status: note.status,
+        date: howManyDays(...dayMonth),
+        howMany: createStringDate(+howManyDays(...dayMonth)),
+        labels: note.labels,
+        } as Note
+   } 
+
+    const applyFilter = (note: Note) : boolean => {
+        return note.status == 0 ? true : false
+   }
+
+    const getNotes = async (filter: boolean = true) => {
+        const db = await connectDB()
+        const keys = await db.keys()
+
+        const newNotes: Note[] = []
+
+        for (let key of keys) {
+            let tempNote = await db.get(key)
+            if (filter) {
+                if (applyFilter(tempNote)) {
+                    newNotes.push(createNote(tempNote, key))
+                }
+            } else {
+                newNotes.push(createNote(tempNote, key))
+            }
+        }
+
+        setNotes(newNotes);
+    }
+
+    const createStringDate = (days: number) => {
+        if (days < 8)
+            return days > 1 ? days + " days" : days + " day"
+        // TODO: change color of "how much times has passed" string as:
+                //9 days, 1 week and 2 days, so light color
+                //12 days, less than 2 weeks but almost there, so darker color
+        else
+            var int = Math.round(days / 7)
+            return int > 1 ? int + " weeks" : int + " week"
+    }
+
+    const howManyDays = (day: number, month: number) => {
+        let today = new Date().getDate()
+        let monthf = new Date().getMonth()
+
+        today = 31 * monthf + today
+        let compare = 31 * month + day
+
+        return (today - compare).toString()
+    }
+
+    const generateRandomInt = (int: number) => {
+        return Math.floor(Math.random() * int)
+    }
+
+    const createLabel = async (label: string) => {
+        const db = await connectDB()
+        let labels: Array<string> = await db.get('labels')
+
+        if (!labels)
+            db.set('labels', [label])
+        else if (labels && labels.includes(label))
+            return
+        else
+            db.set('labels', [...labels, label])
+
+        setLabels(await db.get('labels'))
+    }
+
+    const getLabels = async () =>
+    {
+        const db = await connectDB()
+        setLabels(await db.get('labels'))
+    }
+
+    return {
+        addNote, truncate, getNotes, updateNote, getNote,
+        completeNote, getLabels, createLabel,
+        notes, Priority, Status, labels
+    }
+
+}
+
+export interface Note {
+    key: string,
+    note: string;
+    priority: number,
+    status: number,
+    date: string;
+    howMany: string;
+    labels: [];
+}
+
+export interface NoteDB {
+    note: string;
+    priority: number,
+    status: number,
+    date: string;
+    labels: [];
+}
+
+export enum Priority {
+    Baixa = 0,
+    Média = 1,
+    Alta = 2,
+    //__LENGTH
+}
+
+export enum Status {
+    Pendent = 0,
+    Completed = 1
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,185 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput,
+          IonItem, IonList, IonLabel, IonButton, IonPopover, IonSelect, 
+          IonSelectOption, IonFooter, IonMenuButton, IonButtons
+        } from '@ionic/react';
+import React, { useState, useRef } from 'react';
+
+import { add, remove } from 'ionicons/icons';
+import { Connect, Note, NoteDB } from '../hooks/connect';
+
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
+
+  const { addNote, getNotes, truncate, getNote, updateNote,
+          completeNote, createLabel,
+            Status, Priority, notes, labels } = Connect();
+
+  const pop = useRef<HTMLIonPopoverElement>(null);
+  const input = useRef<HTMLIonInputElement>(null);
+  const select = useRef<HTMLIonSelectElement>(null);
+
+  const [itemInfos, setItemInfos] = useState<Note>({} as Note);
+  const [popoverOpen, setPopoverOpen] = useState(false);
+
+  const colors: { [index: string]: string } =
+  { 0: "low", 1: "mid", 2: "high" }
+
+  const defineColor = (priority: number, days: string) => {
+    return colors[priority] + (+days < 8 ? '-1' : +days > 21 ? '-3' : '-2')
+  }
+
+  const sortArrayAsc = (ar: Note[]) => {
+    return [...notes].sort((a, b) => +a.date! < +b.date! ? 1 : -1, );
+  }
+
+  const openPopover = (e: any, note: Note) => {
+    setItemInfos(note)
+
+    if (!popoverOpen)
+      setPopoverOpen(true);
+  };
+
+  const closePopover = (e: any) => {
+    if (popoverOpen)
+      setPopoverOpen(false);
+  }
+
+  const sendUpdate = (key: string) => {
+    updateNote(key,
+      {
+      note: input.current?.value,
+      priority: select.current?.value,
+      status: 0
+    } as NoteDB)
+  }
+
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonButtons slot="start">
+            <IonMenuButton autoHide={false}></IonMenuButton>
+          </IonButtons>
+          <IonTitle>Tecle</IonTitle>
+          {labels?.map((label: string, index: number) => (
+          <IonButtons key={index} slot="primary">
+            <IonButton fill="outline">
+              {label}
+            </IonButton>
+          </IonButtons>
+          ))}
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+
+      <IonContent>
+        <IonList lines="none">
+          {(sortArrayAsc(notes)).map((note, index) => (
+
+          <IonItem key={+note.key!} 
+                  className={defineColor(note.priority!, note.date!)}
+                  button={true} 
+                  onClick={(e) => {openPopover(e, note)}}>
+
+            <IonLabel class='how-much'>{note.howMany}
+            </IonLabel>
+            <IonLabel id={note.key!}>
+              {note.note}
+            </IonLabel>
+          </IonItem>
+          ))}
+        </IonList>
+
+        <IonPopover isOpen={popoverOpen} ref={pop}
+            //onDidDismiss={closePopover}
+            onIonPopoverDidDismiss={closePopover}
+            reference='event' side='bottom' alignment='center'
+            backdropDismiss={true} 
+            >
+          <IonContent class="ion-padding">
+            <IonItem>
+
+              <IonInput 
+                ref={input} value={itemInfos.note} type='text'
+                onIonInput={(e) => setItemInfos(
+                  {...itemInfos,
+                  note: e.target.value?.toString() ?? ""}
+              )}>
+              </IonInput>
+            </IonItem>
+
+            <IonList>
+              <IonItem>
+                <IonSelect ref={select}
+                   label="Prioridade" interface="popover"
+                   expandedIcon={remove} labelPlacement='stacked' 
+                   value={itemInfos.priority}
+                    onIonChange={(e) => setItemInfos(
+                  {...itemInfos,
+                  priority: (e.target as HTMLIonSelectElement).value}
+              )}>
+
+                  {Object.keys(Priority)
+                         .filter((key) => isNaN(Number(key)))
+                            .map((value, index) => (
+                  <IonSelectOption key={index} value={index}>
+                    {value}
+                  </IonSelectOption>
+                  ))}
+
+                </IonSelect>
+              </IonItem>
+            </IonList>
+
+            <IonButton onClick={(e) => {
+                    completeNote(itemInfos.key)
+                    closePopover(e)}}>
+              Concluir
+            </IonButton>
+
+            <IonButton onClick={(e) => { 
+                    sendUpdate(itemInfos.key) 
+                    closePopover(e)}}>
+              Go
+            </IonButton>
+          </IonContent>
+        </IonPopover>
       </IonContent>
+
+      <IonFooter>
+        <IonToolbar>
+          <IonItem>
+          <IonInput class="main-input" placeholder="Tecle aí" 
+            shape="round" label='>' type='text' defaultValue='kk' onKeyUp={(e) => {
+              if (e.code === "Enter") {
+                let value = (e.target as HTMLInputElement).value;
+                (e.target as HTMLInputElement).value = ''
+
+                if (value == 'drop all') {
+                  truncate()
+                  return
+                } else if (value == 'populate') {
+                  truncate(true)
+                  return
+                } else if (value.includes('label')) {
+                  createLabel(value)
+                  return
+                }
+
+                if (value !== '')
+                  addNote(value)
+              }
+            }
+          }
+            >
+
+          </IonInput>
+          </IonItem>
+        </IonToolbar>
+      </IonFooter>
+
     </IonPage>
   );
 };
